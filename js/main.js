f49eea24a56486810dc6740eaa42f056c97b93fa
KareeMagdyy <karim67899@gmail.com>
2022-03-27 06:14:50 +0200

    Services Section Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let burgerIcon = document.querySelector(".burger-icon");
 let closeNav = document.querySelector(".nav-close");
 let navBar = document.querySelector(".main-nav_links");
+let navLinks = document.querySelectorAll(".main-nav_links li");
 
 burgerIcon.addEventListener("click", () => {
   navBar.style.cssText = "transform:translateX(0)";
@@ -10,4 +11,11 @@ burgerIcon.addEventListener("click", () => {
 closeNav.addEventListener("click", () => {
   navBar.style.cssText = "transform:translateX(100%)";
   burgerIcon.style.display = "flex";
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navBar.style.cssText = "transform:translateX(100%)";
+    burgerIcon.style.display = "flex";
+  });
 });
